907183a5fb846b5f5df1aa5b82881ce23068f54e
cviel <cvielma67@gmail.com>
2026-04-15 18:39:56 -0400

    add dockerfile

--- obj/Debug/net8.0/MiApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MiApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+422ce2783cd3446a7266ae8879fd3ccb6f697aa4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e15d370b95c9638ea7986581575f53334b9f50c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
